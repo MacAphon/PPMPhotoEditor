ef15ab427309523a566d0ca0e2ce1989d7e8c52f
MacAphon <c.a.korn@gmx.de>
2021-04-27 08:38:41 +0200

    first commit
    add files with commit

---
+++ src/com/schule/Main.java
@@ -0,0 +1,12 @@
+package com.schule;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    String activeFile = "images/input.txt"; //args[args.length-1];
+	    Scanner sc = new Scanner(new File(activeFile));
+    }
+}
