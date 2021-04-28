63bc12520e2ae45a80b7e6f14a702d3297e1f58a
MacAphon <c.a.korn@gmx.de>
2021-04-28 19:33:06 +0200

    remove unnessacary import statements add functionality for writing images

--- src/com/schule/Main.java
+++ src/com/schule/Main.java
@@ -1,14 +1,12 @@
 package com.schule;
 
-import java.io.*;
-import java.util.*;
-
 public class Main {
 
     public static void main(String[] args) {
 	    String activeFileIn = args[args.length-2]; //"images/input.txt";
         String activeFileOut = args[args.length-1]; //"images/output.txt";
 
-        PPMImage fileIn = new PPMImage(activeFileIn);
+        PPMImage file = new PPMImage(activeFileIn);
+        file.writeFile(activeFileOut);
     }
 }
--- src/com/schule/PPMImage.java
+++ src/com/schule/PPMImage.java
@@ -4,10 +4,14 @@ import java.io.*;
 import java.util.*;
 
 public class PPMImage {
+    private Pixel[][] image;
+    private int width;
+    private int height;
+    private int cDepth;
 
-    public PPMImage(String fileIn){
+    public PPMImage(String file){
         try {
-            Scanner sc = new Scanner(new File(fileIn));
+            Scanner sc = new Scanner(new File(file));
             String s = sc.next();
 
             if ( !(s.equals("p3") || s.equals("P3")) ){
@@ -15,13 +19,13 @@ public class PPMImage {
                 System.exit(0);
             }
 
-            int width = Integer.parseInt(sc.next());
-            int height = Integer.parseInt(sc.next());
-            int cDepth = Integer.parseInt(sc.next());
+            width = Integer.parseInt(sc.next());
+            height = Integer.parseInt(sc.next());
+            cDepth = Integer.parseInt(sc.next());
 
             int r, g, b;
 
-            Pixel[][] image = new Pixel[height][width];
+            image = new Pixel[height][width];
 
             for(int i = 0; i < height; i++){
                 for(int j = 0; j < width; j++) {
@@ -36,4 +40,28 @@ public class PPMImage {
             e.printStackTrace();
         }
     }
+
+    public void writeFile(String fileOut){
+        try {
+            PrintWriter pw = new PrintWriter(new File(fileOut));
+
+            pw.println("P3");
+            pw.write(String.valueOf(width) + " ");
+            pw.println(String.valueOf(height));
+            pw.println(String.valueOf(cDepth));
+
+            for(int i = 0; i < height; i++){
+                for(int j = 0; j < width; j++) {
+                    pw.write(String.valueOf(image[i][j].getR()) + " ");
+                    pw.write(String.valueOf(image[i][j].getG()) + " ");
+                    pw.write(String.valueOf(image[i][j].getB()) + " ");
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
