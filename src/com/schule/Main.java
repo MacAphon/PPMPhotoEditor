d97be2959dcf0460e63e842c93fcfebfa2f7624f
MacAphon <c.a.korn@gmx.de>
2021-04-28 18:33:25 +0200

    move image reading code to class PPMImage

--- src/com/schule/Main.java
+++ src/com/schule/Main.java
@@ -9,28 +9,6 @@ public class Main {
 	    String activeFileIn = args[args.length-2]; //"images/input.txt";
         String activeFileOut = args[args.length-1]; //"images/output.txt";
 
-        try {
-            Scanner sc = new Scanner(new File(activeFileIn));
-            PrintWriter pw = new PrintWriter(activeFileOut);
-            String s = sc.next();
-
-            if ( !(s.equals("p3") || s.equals("P3")) ){
-                System.out.println("Selected input file is not a valid PPM file. Aborting");
-                System.exit(0);
-            }
-
-            int width = Integer.parseInt(sc.next());
-            int height = Integer.parseInt(sc.next());
-            int cDepth = Integer.parseInt(sc.next());
-
-            while(sc.hasNext()){
-                s = sc.next();
-                pw.write(s + " ");
-            }
-            sc.close();
-            pw.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        PPMImage fileIn = new PPMImage(activeFileIn, activeFileOut);
     }
 }
--- src/com/schule/PPMImage.java
+++ src/com/schule/PPMImage.java
@@ -1,4 +1,34 @@
 package com.schule;
 
+import java.io.*;
+import java.util.*;
+
 public class PPMImage {
+    public PPMImage(String fileIn, String fileOut){
+        try {
+            Scanner sc = new Scanner(new File(fileIn));
+            PrintWriter pw = new PrintWriter(fileOut);
+            String s = sc.next();
+
+            if ( !(s.equals("p3") || s.equals("P3")) ){
+                System.out.println("Selected input file is not a valid PPM file. Aborting");
+                System.exit(0);
+            }
+
+            int width = Integer.parseInt(sc.next());
+            int height = Integer.parseInt(sc.next());
+            int cDepth = Integer.parseInt(sc.next());
+
+            Pixel[][] image = new Pixel[height][width];
+
+            while(sc.hasNext()){
+                s = sc.next();
+                pw.write(s + " ");
+            }
+            sc.close();
+            pw.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
