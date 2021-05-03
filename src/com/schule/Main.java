626efb6afebf0f7c5a2be6d08764dcb898110b4c
MacAphon <c.a.korn@gmx.de>
2021-05-03 13:16:52 +0200

    major rewrite of blur function to allow more advanced filters - currently does not work

--- src/com/schule/Main.java
+++ src/com/schule/Main.java
@@ -3,23 +3,68 @@ package com.schule;
 public class Main {
 
     public static void main(String[] args) {
-	    String activeFileIn = args[args.length-2];
-        String activeFileOut = args[args.length-1];
-        PPMImage file = new PPMImage(activeFileIn);
+	    String fileI = args[args.length-2];
+        String fileO = args[args.length-1];
+        PPMImg file = PPMImgIO.readImg(fileI);
 
         // optional command line arguments
         for (int i=0; i< args.length-2; i++){
             switch (args[i]) {
-                case "--red-channel" -> file.channel("red");
-                case "--green-channel" -> file.channel("green");
-                case "--blue-channel" -> file.channel("blue");
-                case "--invert-colors" -> file.invertColors();
-                case "--bw-soft" -> file.bw("soft");
-                case "--bw-hard" -> file.bw("hard");
-                case "--blur" -> file.blur();
+                case "--red-channel":
+                    file.channel("red");
+                    break;
+                case "--green-channel":
+                    file.channel("green");
+                    break;
+                case "--blue-channel":
+                    file.channel("blue");
+                    break;
+                case "--invert-colors":
+                    file.invertColors();
+                    break;
+                case "--bw-soft":
+                    file.bw("soft");
+                    break;
+                case "--bw-hard":
+                    file.bw("hard");
+                    break;
+                case "--gauss-blur":
+                    file.kernel(new int[][]{
+                                    {1, 4, 6, 4,1},
+                                    {4,16,24,16,4},
+                                    {6,24,36,24,6},
+                                    {4,16,24,16,4},
+                                    {1, 4, 6, 4,1}},
+                            null, 256);
+                    break;
+                case "--edge-detect":
+                    //TODO
+                    file.kernel(new int[][]{
+                                    {1, 4, 0,- 4,-1},
+                                    {4,16, 0,-16,-4},
+                                    {6,24, 0,-24,-6},
+                                    {4,16, 0,-16,-4},
+                                    {1, 4, 0,- 4,-1}},
+                            new int[][]{
+                                    { 1,  4,  6,  4, 1},
+                                    { 4, 16, 24, 16, 4},
+                                    { 0,  0,  0,  0, 0},
+                                    {-4,-16,-24,-16,-4},
+                                    {-1,- 4,- 6,- 4,-1}},
+                            1);
+                    break;
+                case "--sharpen":
+                    file.kernel(new int[][]{
+                                    {-1,- 4,-  6,- 4,-1},
+                                    {-4,-16,- 24,-16,-4},
+                                    {-6,-24, 219,-24,-6},
+                                    {-4,-16,- 24,-16,-4},
+                                    {-1,- 4,-  6,- 4,-1}},
+                            null, 1);
+                    break;
             }
         }
 
-        file.writeFile(activeFileOut);
+        PPMImgIO.writeImg(fileO, file);
     }
 }
---
+++ src/com/schule/PPMImg.java
@@ -0,0 +1,171 @@
+package com.schule;
+
+import java.math.*;
+import java.util.*;
+
+public class PPMImg {
+    private Pixel[][] img;
+    private Pixel[][] img2;
+    private int[][] red;
+    private int[][] green;
+    private int[][] blue;
+    private final int width;
+    private final int height;
+    private final int cDepth;
+
+    public PPMImg(int width, int height, int cDepth, Pixel[][] image){
+        this.width = width;
+        this.height = height;
+        this.cDepth = cDepth;
+        img = image;
+        img2 = image;
+    }
+
+    public int getWidth() { return width; }
+    public int getHeight() { return height; }
+    public int getCDepth() { return cDepth; }
+    public Pixel getPixel(int y, int x) { return img[y][x]; }
+    public Pixel[][] getPixel(){ return img; }
+
+
+    public void channel(String color){
+        switch (color) {
+            case "red":
+                for (int i = 0; i < height; i++) {
+                    for (int j = 0; j < width; j++) {
+                        img[i][j].setG(0);
+                        img[i][j].setB(0);
+                    }
+                }
+                break;
+            case "green":
+                for (int i = 0; i < height; i++) {
+                    for (int j = 0; j < width; j++) {
+                        img[i][j].setR(0);
+                        img[i][j].setB(0);
+                    }
+                }
+                break;
+            case "blue":
+                for (int i = 0; i < height; i++) {
+                    for (int j = 0; j < width; j++) {
+                        img[i][j].setR(0);
+                        img[i][j].setG(0);
+                    }
+                }
+                break;
+        }
+    }
+
+    public void invertColors() {
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                img[i][j].setR(cDepth - img[i][j].getR());
+                img[i][j].setG(cDepth - img[i][j].getG());
+                img[i][j].setB(cDepth - img[i][j].getB());
+            }
+
+        }
+    }
+
+    public void bw(String mode){
+        switch (mode){
+
+            case "soft":
+                for (int i = 0; i < height; i++) {
+                    for (int j = 0; j < width; j++) {
+                        double rLoc = img[i][j].getR();
+                        double gLoc = img[i][j].getG();
+                        double bLoc = img[i][j].getB();
+                        int av = (int) (rLoc+gLoc+bLoc)/3;
+                        img[i][j].setR(av);
+                        img[i][j].setG(av);
+                        img[i][j].setB(av);
+                    }
+
+                }
+                break;
+
+            case "hard":
+                for (int i = 0; i < height; i++) {
+                    for (int j = 0; j < width; j++) {
+                        double rLoc = img[i][j].getR();
+                        double gLoc = img[i][j].getG();
+                        double bLoc = img[i][j].getB();
+                        int av = (int) (rLoc+gLoc+bLoc)/3;
+                        if(av > cDepth/2){
+                            img[i][j].setR(cDepth);
+                            img[i][j].setG(cDepth);
+                            img[i][j].setB(cDepth);
+                        } else {
+                            img[i][j].setR(0);
+                            img[i][j].setG(0);
+                            img[i][j].setB(0);
+                        }
+                    }
+                }
+                break;
+        }
+    }
+    //TODO
+    public void kernel(int[][] vals, int[][] vals2, int value){
+        int[][] rVal = new int[5][5];
+        int[][] gVal = new int[5][5];
+        int[][] bVal = new int[5][5];
+        int[][] val = new int[5][5];
+        int[][] val2 = new int[5][5];
+        int vert;
+        int horiz;
+
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+
+                for (int k = 0; k < 5; k++){
+                    for (int l = 0; l < 5; l++){
+
+                        if(vals2 != null){
+                            try {
+                                val[k][l] = (int) (img[i + k - 2][j + l - 2].getR()
+                                        + img[i + k - 2][j + l - 2].getR()
+                                        + img[i + k - 2][j + l - 2].getR()) / 3 * vals[k][l];
+                                val2[k][l] = (int) (img[i + k - 2][j + l - 2].getR()
+                                        + img[i + k - 2][j + l - 2].getR()
+                                        + img[i + k - 2][j + l - 2].getR()) / 3 * vals2[k][l];
+                            }catch (IndexOutOfBoundsException e){
+                                val[k][l] = 0;
+                                val2[k][l] = 0;
+                            }
+                        } else {
+                            try{
+                                rVal[k][l] = img[i+k-2][j+l-2].getR() * vals[k][l];
+                                gVal[k][l] = img[i+k-2][j+l-2].getG() * vals[k][l];
+                                bVal[k][l] = img[i+k-2][j+l-2].getB() * vals[k][l];
+                            }catch (IndexOutOfBoundsException e) {
+                                rVal[k][l] = 0;
+                                gVal[k][l] = 0;
+                                bVal[k][l] = 0;
+                                value -= vals[k][l];
+                            }
+                        }
+                    }
+                }
+                if(vals2 != null){
+                    vert = (int) Math.pow(Arrays.stream(val).flatMapToInt(Arrays::stream).average().getAsDouble(), 2);
+                    horiz = (int) Math.pow(Arrays.stream(val2).flatMapToInt(Arrays::stream).average().getAsDouble(), 2);
+                    img2[i][j].setRGB((int) Math.sqrt(vert + horiz));
+                } else {
+                    img2[i][j].setR((int) Arrays.stream(rVal).flatMapToInt(Arrays::stream).average().getAsDouble() / value);
+                    img2[i][j].setG((int) Arrays.stream(gVal).flatMapToInt(Arrays::stream).average().getAsDouble() / value);
+                    img2[i][j].setB((int) Arrays.stream(bVal).flatMapToInt(Arrays::stream).average().getAsDouble() / value);
+                }
+            }
+        }
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                img[i][j].setR(img2[i][j].getR());
+                img[i][j].setG(img2[i][j].getG());
+                img[i][j].setB(img2[i][j].getB());
+            }
+        }
+    }
+}
